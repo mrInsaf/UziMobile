d8efa728ef57ab4640aaf8268bad033cfd7ab217
mrInsaf <mr.insaf@list.ru>
2025-04-28 16:07:27 +0300

    1. Убрал апишный сплэш скрин из библиотеки андроида, заменив на пустой экран 2. Убрал динамическое определение startDestination, так как при изменении authState NavGraph перестраивается и автоматически переходит по новому startDestination. Сейчас навигация, связанная с авторизацией происходит централизованно в AppNavigation

--- app/src/main/java/com/example/uzi/ui/MainActivity.kt
+++ app/src/main/java/com/example/uzi/ui/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.example.uzi.ui.screens.AppNavigation
@@ -22,20 +20,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splash = installSplashScreen()
-
         super.onCreate(savedInstanceState)
 
         val authorisationViewModel: AuthorisationViewModel by viewModels()
-        splash.setKeepOnScreenCondition {
-            when (authorisationViewModel.authState.value) {
-                AuthorisationViewModel.AuthState.Loading -> true
-                else -> false
-            }
-        }
+        println(authorisationViewModel)
 
         enableEdgeToEdge()
-
 
         val registrationViewModel: RegistraionViewModel by viewModels()
         val newDiagnosticViewModel: NewDiagnosticViewModel by viewModels()
@@ -43,20 +33,13 @@ class MainActivity : ComponentActivity() {
         val diagnosticListViewModel: DiagnosticListViewModel by viewModels()
 
         setContent {
-            val authState by authorisationViewModel.authState.collectAsState()
-            val startDestination = when (authState) {
-                AuthorisationViewModel.AuthState.Authorized -> AuthScreens.MainScreen.route
-                AuthorisationViewModel.AuthState.Unauthorized -> AuthScreens.AuthorisationScreen.route
-                else -> AuthScreens.SplashScreen.route
-            }
-
             UziTheme(dynamicColor = false) {
                 val navController = rememberNavController()
                 val authorisationUiState by authorisationViewModel.uiState.collectAsStateWithLifecycle()
 
                 AppNavigation(
                     navController = navController,
-                    startDestination = startDestination,
+                    startDestination = AuthScreens.SplashScreen.route,
                     authorisationUiState = authorisationUiState,
                     authorisationViewModel = authorisationViewModel,
                     registrationViewModel = registrationViewModel,
